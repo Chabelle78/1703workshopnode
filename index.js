20f6b07d0b23cc4b936b1f1255c1d1298cfed90f
Chabelle78 <chabat.estelle@gmail.com>
2021-03-17 16:32:08 +0100

    Ajout de la constante + couleur via chalk

---
+++ index.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk");
+
+
+
+console.log(chalk.blue("estelle"));
+
+console.log(chalk.orange("jacques"));
+console.log(chalk.green("victoria"));
+console.log(chalk.grey("ben"));
+
+//ok ok 
